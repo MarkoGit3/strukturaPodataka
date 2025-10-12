cec24fbdebc9d34a55cafb735d5db27b81e4c146
MarkoGit3 <lel>
2025-10-12 21:05:10 +0200

    prazniProgram

---
+++ vjezba1/Kod/Kod/vjezba.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*(struct _Person;
+typedef struct _Person* Position;
+typedef struct _Person
+{
+	char name[MAX_LENGTH];
+	char surname[MAX_LENGTH];
+	int birthYear;
+	Position next;
+} Person;)*/
+
+int main() {
+	printf("Proba..\n");
+
+	return 0;
+}
